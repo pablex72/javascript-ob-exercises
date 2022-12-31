d28e430fe40306eeba29d0b565ea522ad4a2b001
pablex72 <tonyp2hoy@gmail.com>
2022-12-30 22:27:11 -0400

    openbootcamp sintaxis

---
+++ 2-Sintaxis/sintaxis_ob_exer.js
@@ -0,0 +1,14 @@
+const myList = [
+    "Anthony",
+    22,
+    true,
+    new Date(2000, 11, 11),
+    {
+        title: "You can success",
+        autor: "Lucas John",
+        date: new Date(1999, 1, 1),
+        url: "https://myBooks.book"
+    }
+]
+
+console.log(myList.title);
