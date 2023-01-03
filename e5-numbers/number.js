7bde789b4b403fa1774058345d43defd60baa5b6
pablex72 <tonyp2hoy@gmail.com>
2023-01-03 17:31:28 -0400

    openbootcamp number

---
+++ e5-numbers/number.js
@@ -0,0 +1,10 @@
+const height_cm = 170
+const height_m = 1.70
+const weight_kg = 80.2
+
+const height_red = Math.ceil(height_m)
+const weight_red = Math.floor(weight_kg)
+console.log(height_red , weight_red)
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(sonIguales)
